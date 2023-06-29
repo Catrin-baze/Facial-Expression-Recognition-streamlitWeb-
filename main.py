0fe76c4637d21d96be256cb6a931603dbf25ad72
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2023-06-29 18:47:05 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,8 +31,8 @@ st.write("")
 st.write("Just a second...")
 img = mmcv.imread(file_up)
 model = init_model(
-    config="configs/apvit/RAF.py",
-    checkpoint="weights/APViT_RAF-3eeecf7d.pth"
+    config="/mmcls/configs/apvit/RAF.py",
+    checkpoint="/mmcls/weights/APViT_RAF-3eeecf7d.pth"
 )
 result = inference_model(model, img)
 st.success('successful prediction')
