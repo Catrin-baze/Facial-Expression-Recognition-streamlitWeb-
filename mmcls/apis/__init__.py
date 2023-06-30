665b0b37ddf03d95de9ea548b5dbc536d18dd09c
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2023-06-30 12:52:46 +0800

    Update __init__.py

--- mmcls/apis/__init__.py
+++ mmcls/apis/__init__.py
@@ -1,5 +1,5 @@
 from .inference import inference_model, init_model, show_result_pyplot
-from .test import multi_gpu_test, single_gpu_test
+#from .test import multi_gpu_test, single_gpu_test
 #from .train import set_random_seed, train_model
 
 #__all__ = [
