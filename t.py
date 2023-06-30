35b4f61131b411af464f163b20d71568d51327cf
Catrin-baze <92102923+Catrin-baze@users.noreply.github.com>
2023-06-30 18:40:11 +0800

    Update t.py

--- t.py
+++ t.py
@@ -1,147 +1,32 @@
-from io import StringIO
-from pathlib import Path
 import streamlit as st
-import time
-from yolo5.detect import detect
+import numpy as np
+import pandas as pd
 from fer import FER
-import os
-import sys
-import argparse
 from PIL import Image
+import cv2
 
-def draw_result(frame=None, result=None):
-    if result:
-        result_max = max(result[0]["emotions"].items(), key=lambda x: x[1])
-        emotion_type, emotion_value = result_max
-        text = emotion_type + ":" + str(emotion_value)
 
-        x, y, w, h = result[0]["box"]
-        point_top = np.array([x, y])
-        point_bot = np.array([x + w, y + h])
 
-        point_color = (0, 255, 0)
-        thickness = 2
-        lineType = 4
 
-        cv2.rectangle(frame, tuple(point_top), tuple(point_bot), point_color, thickness, lineType)
-        t_size = cv2.getTextSize(text, 1, cv2.FONT_HERSHEY_PLAIN, 1)[0]  # 获取文字框大小
-        text_bot = point_top + np.array(list(t_size))
-        cv2.rectangle(frame, tuple(point_top), tuple(text_bot), point_color, -1)
-
-        # 计算文字偏移
-        point_top[1] = point_top[1] + (t_size[1] / 2 + 4)
-
-        cv2.putText(frame, text, tuple(point_top), cv2.FONT_HERSHEY_PLAIN, 1.0, (255, 0, 255), 1)
-
+@st.cache
+def getEmotions(img):
+    detector  = FER(mtcnn=True)
+    result = detector.detect_emotions(img)
+    data  = result[0]['emotions']
+    if data is None:
+        st.write('No result')
+        return False
     else:
-        print("detect None")
+        return data
 
-    return frame
+st.write('This is an app to return emotions of image')
 
-def get_subdirs(b='.'):
-    '''
-        Returns all sub-directories in a specific Path
-    '''
-    result = []
-    for d in os.listdir(b):
-        bd = os.path.join(b, d)
-        if os.path.isdir(bd):
-            result.append(bd)
-    return result
+file = st.sidebar.file_uploader('Please upload an image file', type = ['jpg','png'])
 
-
-def get_detection_folder():
-    '''
-        Returns the latest folder in a runs\detect
-    '''
-    return max(get_subdirs(os.path.join('runs', 'detect')), key=os.path.getmtime)
-
-
-if __name__ == '__main__':
-
-    st.title('YOLOv5 Streamlit App')
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--weights', nargs='+', type=str,
-                        default='weights/yolov5s.pt', help='model.pt path(s)')
-    parser.add_argument('--source', type=str,
-                        default='data/images', help='source')
-    parser.add_argument('--img-size', type=int, default=640,
-                        help='inference size (pixels)')
-    parser.add_argument('--conf-thres', type=float,
-                        default=0.35, help='object confidence threshold')
-    parser.add_argument('--iou-thres', type=float,
-                        default=0.45, help='IOU threshold for NMS')
-    parser.add_argument('--device', default='',
-                        help='cuda device, i.e. 0 or 0,1,2,3 or cpu')
-    parser.add_argument('--view-img', action='store_true',
-                        help='display results')
-    parser.add_argument('--save-txt', action='store_true',
-                        help='save results to *.txt')
-    parser.add_argument('--save-conf', action='store_true',
-                        help='save confidences in --save-txt labels')
-    parser.add_argument('--nosave', action='store_true',
-                        help='do not save images/videos')
-    parser.add_argument('--classes', nargs='+', type=int,
-                        help='filter by class: --class 0, or --class 0 2 3')
-    parser.add_argument('--agnostic-nms', action='store_true',
-                        help='class-agnostic NMS')
-    parser.add_argument('--augment', action='store_true',
-                        help='augmented inference')
-    parser.add_argument('--update', action='store_true',
-                        help='update all models')
-    parser.add_argument('--project', default='runs/detect',
-                        help='save results to project/name')
-    parser.add_argument('--name', default='exp',
-                        help='save results to project/name')
-    parser.add_argument('--exist-ok', action='store_true',
-                        help='existing project/name ok, do not increment')
-    opt = parser.parse_args()
-    print(opt)
-
-    source = ("图片检测", "视频检测")
-    source_index = st.sidebar.selectbox("选择输入", range(
-        len(source)), format_func=lambda x: source[x])
-
-    if source_index == 0:
-        uploaded_file = st.sidebar.file_uploader(
-            "上传图片", type=['png', 'jpeg', 'jpg'])
-        if uploaded_file is not None:
-            is_valid = True
-            with st.spinner(text='资源加载中...'):
-                st.sidebar.image(uploaded_file)
-                picture = Image.open(uploaded_file)
-                picture = picture.save(f'data/images/{uploaded_file.name}')
-                opt.source = f'data/images/{uploaded_file.name}'
-        else:
-            is_valid = False
-    else:
-        uploaded_file = st.sidebar.file_uploader("上传视频", type=['mp4'])
-        if uploaded_file is not None:
-            is_valid = True
-            with st.spinner(text='资源加载中...'):
-                st.sidebar.video(uploaded_file)
-                with open(os.path.join("data", "videos", uploaded_file.name), "wb") as f:
-                    f.write(uploaded_file.getbuffer())
-                opt.source = f'data/videos/{uploaded_file.name}'
-        else:
-            is_valid = False
-
-    if is_valid:
-        print('valid')
-        if st.button('开始检测'):
-
-            detect(opt)
-
-            if source_index == 0:
-                with st.spinner(text='Preparing Images'):
-                    for img in os.listdir(get_detection_folder()):
-                        st.image(str(Path(f'{get_detection_folder()}') / img))
-
-                    st.balloons()
-            else:
-                with st.spinner(text='Preparing Video'):
-                    for vid in os.listdir(get_detection_folder()):
-                        st.video(str(Path(f'{get_detection_folder()}') / vid))
-
-                    st.balloons()
+if file is None:
+    st.write("You haven't uploaded an image file")
+else:
+    image = Image.open(file)
+    img = np.array(image)
+    st.image(image, use_column_width=True)
+    st.write(pd.DataFrame(getEmotions(img), index=[0]))
